6856bec12d3f9e07c4d56a66cedf59aa7e9d41e0
Stivin <stroykin.king@gmail.com>
2026-06-16 17:55:32 +0300

    feat: add submission

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,6 +3,7 @@ import { TuiCard, TuiForm, TuiHeader } from '@taiga-ui/layout';
 import {
   TuiButton,
   TuiCheckbox,
+  TuiError,
   TuiFilterByInputPipe,
   TuiGroup,
   TuiInput,
@@ -10,9 +11,10 @@ import {
   TuiSelectLike,
   TuiTitle,
 } from '@taiga-ui/core';
-import { form, FormField, minLength, required } from '@angular/forms/signals';
+import { disabled, form, FormField, FormRoot, required } from '@angular/forms/signals';
 import {
   TuiBlock,
+  TuiButtonLoading,
   TuiChevron,
   TuiComboBox,
   TuiDataListWrapper,
@@ -31,6 +33,45 @@ import {
   TuiTextarea,
 } from '@taiga-ui/kit';
 import { JsonPipe } from '@angular/common';
+import { TuiDay } from '@taiga-ui/cdk';
+
+interface FormModel {
+  string: string;
+  textarea: string;
+  number: number | null;
+  date: TuiDay | null;
+  phone: string;
+  select: string | null;
+  comboBox: string | null;
+  chips: string[];
+  slider: number;
+  file: TuiFileLike | null;
+  pin: string;
+  color: string;
+  radio: 'option-1' | 'option-2';
+  switch: boolean;
+  firstCheck: boolean;
+  secondCheck: boolean;
+}
+
+const INITIAL_MODEL: FormModel = {
+  string: '',
+  textarea: '',
+  number: null,
+  date: null,
+  phone: '',
+  select: null,
+  comboBox: null,
+  chips: [],
+  slider: 50_000,
+  file: null,
+  pin: '',
+  color: '',
+  radio: 'option-1',
+  switch: false,
+  firstCheck: true,
+  secondCheck: false,
+} as const;
 
 @Component({
   selector: 'app',
@@ -39,6 +80,7 @@ import { JsonPipe } from '@angular/common';
     TuiHeader,
     TuiTitle,
     TuiInput,
+    FormRoot,
     FormField,
     TuiCard,
     TuiInputNumber,
@@ -64,43 +106,44 @@ import { JsonPipe } from '@angular/common';
     TuiFilterByInputPipe,
     TuiInputPin,
     TuiInputColor,
+    TuiButtonLoading,
+    TuiError,
   ],
   templateUrl: './app.html',
   styleUrl: './app.less',
 })
 export class App {
   protected readonly items: string[] = inject('Pythons' as any);
-  protected readonly users = this.items.map((name, index) => ({ name, index }));
+
   protected readonly form = form(
-    signal({
-      string: '',
-      textarea: '',
-      number: null as number | null,
-      date: null,
-      phone: '',
-      select: null as string | null,
-      comboBox: null as string | null,
-      chips: [] as string[],
-      slider: 50_000,
-      file: null as TuiFileLike | null,
-      pin: '',
-      color: '',
-      radio: 'option-1',
-      switch: false,
-      firstCheck: true,
-      secondCheck: false,
-    }),
-    (path) => {
-      required(path.string);
-      required(path.textarea);
-      required(path.number);
-      required(path.date);
-      required(path.phone);
-      required(path.select);
-      required(path.comboBox);
-      required(path.chips);
-      required(path.file);
-      minLength(path.pin, 4);
+    signal({ ...INITIAL_MODEL }),
+    (root) => {
+      disabled(root, { when: () => this.form().submitting() });
+      required(root.string);
+      // required(root.textarea);
+      // required(root.number);
+      // required(root.date);
+      // required(root.phone);
+      // required(root.select);
+      // required(root.comboBox);
+      // required(root.chips);
+      // required(root.file);
+      // minLength(root.pin, 4);
+    },
+    {
+      submission: {
+        action: async (field) => {
+          await new Promise((resolve) => setTimeout(resolve, 3000));
+          field().reset({ ...INITIAL_MODEL });
+        },
+        onInvalid: (field) => {
+          field().errorSummary()[0]?.fieldTree().focusBoundControl();
+        },
+      },
     },
   );
+
+  protected resetForm(): void {
+    this.form().reset({ ...INITIAL_MODEL });
+  }
 }
